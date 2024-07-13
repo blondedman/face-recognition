0ff9f5118373a314b9a77eca8f30475208c9da74
Shankhadeep Maiti <maitishankhadeep123@gmail.com>
2024-07-13 13:58:52 +0530

    updated fonts

--- recognition_image.py
+++ recognition_image.py
@@ -22,8 +22,7 @@ rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 # detect the (x, y) coordinates of the bounding boxes corresponding
 # to each face in the input image, then compute the facial embeddings
 print("[INFO] recognizing faces")
-boxes = face_recognition.face_locations(rgb,
-	model=args["detection_method"])
+boxes = face_recognition.face_locations(rgb, model=args["detection_method"])
 encodings = face_recognition.face_encodings(rgb, boxes)
 
 # initialize the list of names
@@ -65,7 +64,7 @@ for ((top, right, bottom, left), name) in zip(boxes, names):
 	# draw the predicted face name on the image
 	cv2.rectangle(image, (left, top), (right, bottom), (0, 255, 0), 2)
 	y = top - 15 if top - 15 > 15 else top + 15
-	cv2.putText(image, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
+	cv2.putText(image, name, (left, y), cv2.FONT_HERSHEY_DUPLEX, 0.75, (0, 255, 0), 2)
  
 # show the output image
 cv2.imshow("image", image)
